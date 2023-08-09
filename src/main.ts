c790a74f54ab6c813d508c872be227f0ac33b923
Manikanta kantipudi <kantipudimanikanta2000@gmail.com>
2023-08-09 13:03:45 +0530

    images from server issue

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import * as express from 'express';
 import * as dotenv from 'dotenv';
+import * as path from 'path';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   dotenv.config();
   app.enableCors();
+  const staticPath = path.join(__dirname, '..', 'files');
+  app.use('/uploads', express.static(staticPath));
   await app.listen(200);
 }
 bootstrap();
